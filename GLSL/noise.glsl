f03775c6eca0f55127fbe69c74ad9dc1f404abdf
Omer Shapira <info@omershapira.com>
2016-02-27 12:28:34 -0500

    Adds Valve's Screen-space dither

--- GLSL/noise.glsl
+++ GLSL/noise.glsl
@@ -12,3 +12,16 @@ float hash(in ivec2 c)
     
   return float(r)*(2.0/8589934592.0)+0.5;
 }
+
+
+
+//Valve Screen Space Dither
+// https://www.youtube.com/watch?v=JO7G38_pxU4 at 55:45
+//HLSL
+float3 ScreenSpaceDither(float2 vScreenPos)
+{
+	//lestyn's RGB dither (7 asm instructions) from Portal 2 X360, slightly modified for VR
+	float3 vDither = dot (float2(171.0, 231.0), vScreenPos.xy + g_flTime).xxx;
+	vDither.rgb = frac(vDither.rgb) / float3(103.0, 71.0, 97.0)) - float3(0.5, 0.5, 0.5);
+	return (vDither.rgb / 255.0) * 0.375;
+}
